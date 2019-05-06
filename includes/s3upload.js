7d1c6ae27ac469e276669f6bb34f96860df9b58d
Jithin Sebastian <jithin.sebastian@mavapartners.com>
2019-05-06 14:15:07 +0530

    Updating knox code to use aws-sdk

--- includes/s3upload.js
+++ includes/s3upload.js
@@ -1,5 +1,6 @@
 var _ = require('underscore');
-var knox = require('knox');
+var AWS = require('aws-sdk');
+var fs = require('fs');
 var moment = require('moment');
 
 var configOK = function(utils_config) {
@@ -7,7 +8,7 @@ var configOK = function(utils_config) {
 	// Check required config is present and correct (and put in defaults if otherwise)...
 	
 	var err = [];
-	
+
 	if(_.isUndefined(utils_config.s3upload)) {
 		err.push('ci-utils requires a s3upload object.');
 	} else {
@@ -58,38 +59,65 @@ var s3upload = function(config) {
     // confirm config is ok
     utils_config = configOK(utils_config);
 
-		// make amazon s3 thing
-		var filestore = knox.createClient({
-												key: utils_config.s3upload.key, 
-												secret: utils_config.s3upload.secret, 
-												bucket: utils_config.s3upload.bucket,
-												region: utils_config.s3upload.region
-										});
+			// configure AWS SDK
+		AWS.config.update({
+			accessKeyId: utils_config.s3upload.key,
+			secretAccessKey: utils_config.s3upload.secret,
+			region: utils_config.s3upload.region
+		});
+
+		var s3 = new AWS.S3();
+
+		//Using AWS-SDK now.
+		// // make amazon s3 thing
+		// var filestore = knox.createClient({
+		// 										key: utils_config.s3upload.key, 
+		// 										secret: utils_config.s3upload.secret, 
+		// 										bucket: utils_config.s3upload.bucket,
+		// 										region: utils_config.s3upload.region
+		// 								});
 										
-		var params = {"Content-Type": fileType};
+		fs.readFile(fileFrom, function(err, fileData) {
+      var params = {
+        Bucket: utils_config.s3upload.bucket,
+        Key: fileTo,
+        ContentType: fileType,
+        Body: fileData
+			}
+			if (publicRead===true) {
+        params['ACL'] = "public-read";
+			}
+      s3.putObject(params, function(err, data) {
+        if (err) callback(['Problem uploading your file to s3'],{}); //error
+        else callback(null,{"asset_location":utils_config.s3upload.asset_domain.replace(/\/$/, "")+fileTo}); //success
+      });  
+    });
+
+		//Commenting knox code.
+		// var params = {"Content-Type": fileType};
 										
 												
-		if (publicRead===true) {	
-		   params['x-amz-acl']="public-read";
-		}
+		// if (publicRead===true) {	
+		//    params['x-amz-acl']="public-read";
+		// }
                   
-    var req = filestore.putFile(fileFrom, fileTo, params, function(err, res){
+    // var req = filestore.putFile(fileFrom, fileTo, params, function(err, res){
 
-      if(err){
-        //console.log(err);
-        callback(['Problem uploading your file to s3'],{});
-      }
-      else if(res.statusCode == 200){
-        callback(null,{"asset_location":utils_config.s3upload.asset_domain.replace(/\/$/, "")+fileTo});
-      }
-      else{
-        //console.log(res, res.statusCode)
-        //Must be some error
-        //console.log("CATCH ALL ERROR");
-        callback(['Problem uploading your file to s3 - Status Code ('+res.statusCode+')'],{});
-      }
+    //   if(err){
+    //     //console.log(err);
+    //     callback(['Problem uploading your file to s3'],{});
+    //   }
+    //   else if(res.statusCode == 200){
+    //     callback(null,{"asset_location":utils_config.s3upload.asset_domain.replace(/\/$/, "")+fileTo});
+    //   }
+    //   else{
+    //     //console.log(res, res.statusCode)
+    //     //Must be some error
+    //     //console.log("CATCH ALL ERROR");
+    //     callback(['Problem uploading your file to s3 - Status Code ('+res.statusCode+')'],{});
+    //   }
       
-    });
+    // });
 	
 	}
 
